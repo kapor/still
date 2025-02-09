c4ed708d62db43b6cf9650c999198b3a5d1aadd4
Aaron Kapor <aaronkapor@gmail.com>
2025-02-08 17:38:08 -0800

    brought groups over to posgres

--- aaa/aaa/settings.py
+++ aaa/aaa/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'posgres': {
+    'default': {
         "NAME": "still",
         "ENGINE": "django.db.backends.postgresql",
         "USER": "aaronkapor",
--- aaa/accounts/models.py
+++ aaa/accounts/models.py
@@ -5,3 +5,5 @@ from django.contrib import admin, auth
 class User(auth.models.User, auth.models.PermissionsMixin):
 	def __str__(self):
 		return "@{}".format(self.username)
+
+
