831ccadb864f5412a5315821a1c9b4254f38002a
Aaron Kapor <aaronkapor@gmail.com>
2025-03-20 09:19:26 -0700

    ...

--- aaa/groups/views.py
+++ aaa/groups/views.py
@@ -12,22 +12,20 @@ from posts.models import Post
 from django.http import Http404
 from django.views import generic
 from django.db import IntegrityError
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_GET, require_http_methods, require_POST
 from django.http import HttpRequest, HttpResponse, JsonResponse
-from django.shortcuts import render
 from django.forms.models import inlineformset_factory
 from django.forms import modelformset_factory
 
 
 
 
-GroupFormset = modelformset_factory(Post, fields=('message',),)
 
-
-# class SingleGroup(LoginRequiredMixin, DetailView):
-# 	model = Group
+class SingleGroup(LoginRequiredMixin, DetailView):
+	model = Group
 
 
 
@@ -43,11 +41,13 @@ def SingleGroup(request, slug):
 		if form.is_valid():
 			comment = form.save(commit=False)
 			comment.group = group
-			comment.author = request.user
+			comment.user = request.user
 			comment.save()
+			messages.success(request, 'Comment added')
 			return redirect('groups:single', slug=slug)
 	else:
 		form = CommentForm()
+
 		context = {'comment': comment, 'group': group, 'form': form}
 	return render(request, 'groups/group_detail.html', context)
 
@@ -56,14 +56,13 @@ def SingleGroup(request, slug):
 
 
 
-
-
-
+@login_required
 def CommentDelete(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     group_slug = comment.group.slug  # Retrieve the post's slug
     comment.delete()
-    return redirect(reverse('groups:single', kwargs={'slug': group_slug}))
+    messages.success(request, 'Comment deleted')
+    return redirect(request.META['HTTP_REFERER'])
 
 
 
@@ -86,9 +85,9 @@ class JoinGroup(LoginRequiredMixin, RedirectView):
 		try:
 			GroupMember.objects.create(user = self.request.user, group=group)
 		except IntegrityError:
-			messages.warning(self.request, ('Already a member.'))
+			messages.warning(self.request, ('Already a member'))
 		else:
-			messages.success(self.request, ("You're now a member."))
+			messages.success(self.request, ("You're now a member"))
 		return super().get(request, *args, **kwargs)
 
 
@@ -117,38 +116,44 @@ class LeaveGroup(LoginRequiredMixin, RedirectView):
 class GroupView(ListView):
     model = Group
     fields = ('name', 'description', 'members')
-    template_name = "groups/groups.html"
+    template_name = "groups/group.html"
     context_object_name = "grouplist"
     paginate_by = 20
     # ordering = 'pk'
     ordering = ['name']
     # new method added ⬇️
-    def get_template_names(self, *args, **kwargs):
-        if self.request.htmx:
-            return "groups/group_list.html"
-        else:
-            return self.template_name
+    # def get_template_names(self, *args, **kwargs):
+    #     if self.request.htmx:
+    #         return "groups/group_list.html"
+    #     else:
+    #         return self.template_name
 
 
 
 
 
-class GroupPost(LoginRequiredMixin, RedirectView):
-	fields = ('message', 'group')
-	model = Post
-	template_name = 'group_detail_post.html'
-
-	def get_redirect_url(self, *args, **kwargs):
-		return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
-
-	def get(self, request, *args, **kwargs):
-		group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
-		return super().get(request, *args, **kwargs)
-
-	def get_form(self, form_class=None):
-		return GroupFormset(**self.get_form_kwargs(), instance=self.object)
 
 
+## def load_post(request, num_posts):
+def LoadGroup(request, **kwargs):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        num_posts = kwargs.get('num_posts')
+        visible = 40
+        upper = num_posts
+        lower = upper - visible
+        size = Group.objects.all().count()
+
+        qs = Group.objects.all()
+        # you cannot pass in a JSON response as a query set directly – so creating an empty dictionary and looping through an object and appending it to the dictionary is a solution.
+        data = []
+        for item in qs:
+            item = {
+                'slug': item.slug,
+                'name': item.name,
+                'description': item.description,
+            }
+            data.append(item)
+        return JsonResponse({'data':data[lower:upper], 'size':size })
 
 
 
@@ -156,12 +161,13 @@ class GroupPost(LoginRequiredMixin, RedirectView):
 class CreateGroup(LoginRequiredMixin, CreateView):
 	model = Group
 	form_class = GroupForm
-  
+
+	# success_url = reverse_lazy('groups:all')
 
 
 
 # Modal View
-class AddGroup(LoginRequiredMixin,generic.CreateView,):
+class AddGroup(LoginRequiredMixin, generic.CreateView):
 	template_name = 'groups/group_modal.html'
 	model = models.Group
 	context_object_name = 'addgroup'
